559419dc19659e9d9a8b645e1ea410fd55feed39
Rafael Vargas <rafael.s.vargas18@gmail.com>
2021-12-28 19:11:10 -0400

    Relocating help command to Control class

--- vulkanbot/general/Control.py
+++ vulkanbot/general/Control.py
@@ -9,6 +9,11 @@ class Control(commands.Cog):
 
     def __init__(self, bot):
         self.__bot = bot
+        self.__comandos = {
+            'MUSIC': ['this', 'resume', 'pause', 'loop', 'stop', 'skip', 'play', 'queue', 'clear'],
+            'RANDOM': ['cetus', ' frase'],
+            'HELP': ['help']
+        }
 
     @property
     def bot(self):
@@ -32,6 +37,35 @@ class Control(commands.Cog):
             await ctx.channel.send(f'O comando não existe')
         else:
             raise error
+
+    @commands.command(name="help", alisases=['ajuda'], help="Comando de ajuda")
+    async def help(self, ctx):
+        helptxt = ''
+        help_music = '-- MUSIC\n'
+        help_random = '-- RANDOM\n'
+        help_help = '-- HELP\n'
+
+        for command in self.__bot.commands:
+            if command.name in self.__comandos['MUSIC']:
+                help_music += f'**{command}** - {command.help}\n'
+            elif command.name in self.__comandos['HELP']:
+                help_help += f'**{command}** - {command.help}\n'
+            else:
+                help_random += f'**{command}** - {command.help}\n'
+        helptxt = f'{help_music}\n{help_random}\n{help_help}'
+
+        embedhelp = discord.Embed(
+            colour=config.COLOURS['grey'],
+            title=f'Comandos do {self.__bot.user.name}',
+            description=helptxt
+        )
+
+        embedhelp.set_thumbnail(url=self.__bot.user.avatar_url)
+        await ctx.send(embed=embedhelp)
+
+    @commands.Cog.listener()
+    async def on_error(self, error):
+        print('On Error')
 
 
 def setup(bot):
